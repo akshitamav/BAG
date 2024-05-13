dfe9210404d40b1b8de1bb2a73440fef33a83651
Akshita Mavurapu <akshitamavurapu@gmail.com>
2024-05-13 03:09:11 +0000

    update build for debugging
    Signed-off-by: Akshita Mavurapu <akshitamavurapu@gmail.com>

--- fuzzers/build.sh
+++ fuzzers/build.sh
@@ -9,7 +9,7 @@ BUILDTYPE=Debug
 LIBRARY=-lbaglibd
 HDF_END=_debug
 XML_END=
-EXTRA_CXX_FLAGS="-g" "-O0"
+EXTRA_CXX_FLAGS=-g
 
 cmake -G Ninja -DCMAKE_BUILD_TYPE=$BUILDTYPE -B build -S . \
   -DCMAKE_INSTALL_PREFIX:PATH=/opt \
@@ -29,26 +29,24 @@ echo "CXXFLAGS:"
 echo $CXXFLAGS
 
 echo "Building bag_read_fuzzer..."
-$CXX $CXXFLAGS \
+$CXX $CXXFLAGS $EXTRA_CXX_FLAGS \
   -I$SRC_DIR/api \
   fuzzers/bag_read_fuzzer.cpp -o $OUT/bag_read_fuzzer \
   $LIB_FUZZING_ENGINE \
   -L/opt/lib/static $LIBRARY \
   -L/opt/lib -lhdf5_cpp$HDF_END \
   -L/opt/lib -lhdf5$HDF_END \
-  -L/opt/lib -lxml2$XML_END \
-  $EXTRA_CXX_FLAGS
+  -L/opt/lib -lxml2$XML_END
 
 echo "Building bag_extended_fuzzer..."
-$CXX $CXXFLAGS \
+$CXX $CXXFLAGS $EXTRA_CXX_FLAGS \
   -I$SRC_DIR/api \
   fuzzers/bag_extended_fuzzer.cpp -o $OUT/bag_extended_fuzzer \
   $LIB_FUZZING_ENGINE \
   -L/opt/lib/static $LIBRARY \
   -L/opt/lib -lhdf5_cpp$HDF_END \
   -L/opt/lib -lhdf5$HDF_END \
-  -L/opt/lib -lxml2$XML_END \
-  $EXTRA_CXX_FLAGS
+  -L/opt/lib -lxml2$XML_END
 
 echo "Building seed corpus..."
 zip -j $OUT/bag_extended_fuzzer_seed_corpus.zip $SRC_DIR/examples/sample-data/*.bag
