91c0c1b7cb043812d07f3c9b60618c1b4582661a
Akshita Mavurapu <akshitamavurapu@gmail.com>
2024-05-13 02:47:53 +0000

    update build for debugging
    Signed-off-by: Akshita Mavurapu <akshitamavurapu@gmail.com>

--- fuzzers/build.sh
+++ fuzzers/build.sh
@@ -14,6 +14,15 @@ cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -B build -S . \
 
 cmake --build build --config Debug --target install
 
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build -S . \
+  -DCMAKE_INSTALL_PREFIX:PATH=/opt \
+  -DCMAKE_PREFIX_PATH='/opt;/opt/local;/opt/local/HDF_Group/HDF5/1.14.3/' \
+  -DBAG_BUILD_SHARED_LIBS:BOOL=OFF \
+  -DBAG_BUILD_TESTS:BOOL=OFF -DBAG_CODE_COVERAGE:BOOL=OFF \
+  -DBAG_BUILD_PYTHON:BOOL=OFF -DBAG_BUILD_EXAMPLES:BOOL=OFF
+
+cmake --build build --config Release --target install
+
 echo $(pkg-config baglib)
 echo $(ldconfig -p | grep "bag")
 ls /opt/lib/static
@@ -23,7 +32,7 @@ $CXX $CXXFLAGS \
   -I$SRC_DIR/api \
   fuzzers/bag_read_fuzzer.cpp -o $OUT/bag_read_fuzzer \
   $LIB_FUZZING_ENGINE \
-  -L/opt/lib/static -lbaglibd \
+  -L/opt/lib/static -lbaglib \
   -L/opt/lib -lhdf5_cpp \
   -L/opt/lib -lhdf5 \
   -L/opt/lib -lxml2
